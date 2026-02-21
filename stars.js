fc99e79f79bbcf12f7bc15da7431267b74ec54c1
grvrjt <gaurav1993rajput@gmail.com>
2026-02-21 16:50:38 +0530

    more stars pattern added

--- stars.js
+++ stars.js
@@ -71,3 +71,26 @@ for (let i = 0; i < 4; i++) {
     }
     console.log(row);
 }
+
+/*
+--------
+1234
+123
+12
+1
+--------
+*/
+for (let i = 4; i > 0; i--) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+        row = row + (j + 1);
+    }
+    console.log(row)
+}
+for (let i = 0; i < 4; i++) {
+    let row = "";
+    for (let j = 0; j < 4 - i; j++) {
+        row = row + (j + 1);
+    }
+    console.log(row)
+}
