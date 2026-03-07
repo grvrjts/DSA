e01ddd6f1fe7ba0a7f6e9c64d135959186df3c6d
grvrjt <grvrjt@dell-Inspiron-3542.lan>
2026-03-07 01:53:45 -0500

    reverse integer added-updated

--- reverseInt.js
+++ reverseInt.js
@@ -4,10 +4,12 @@ function reverseInt(x) {
     x = Math.abs(x);
     while (x > 0) {
         let lastDigit = x % 10;
-        rev = (rev * 10 )+ lastDigit;
-        x = Math.floor(x/10);
+        rev = (rev * 10) + lastDigit;
+        x = Math.floor(x / 10);
     }
-    return xCopy < 0 ? -rev : rev;
+    let limit = Math.pow(2, 31);
+    if (rev < -limit || rev > limit) return 0;
+        return xCopy < 0 ? -rev : rev;
 }
 let n = -12345;
 console.log(reverseInt(n));
