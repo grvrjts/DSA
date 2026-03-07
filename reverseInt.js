8cd147a662f1ca4aaa5f069ea6d259c550160fff
grvrjt <grvrjt@dell-Inspiron-3542.lan>
2026-03-07 01:45:33 -0500

    reverse integer added

---
+++ reverseInt.js
@@ -0,0 +1,13 @@
+function reverseInt(x) {
+    let rev = 0;
+    let xCopy = x;
+    x = Math.abs(x);
+    while (x > 0) {
+        let lastDigit = x % 10;
+        rev = (rev * 10 )+ lastDigit;
+        x = Math.floor(x/10);
+    }
+    return xCopy < 0 ? -rev : rev;
+}
+let n = -12345;
+console.log(reverseInt(n));
