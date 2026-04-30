a45a2656e4bcb0a69ed0651cf2cbbe4507fddb31
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-04-30 11:08:20 +0530

    binary search added

---
+++ Searching&Sorting/binarySearch.js
@@ -0,0 +1,15 @@
+function binarySearch(arr, target) {
+    let left =0;
+    let right = arr.length -1;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] === target){
+            return mid; // Return the index of the target element
+        } else if(arr[mid] < target){
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target element not found
+}
