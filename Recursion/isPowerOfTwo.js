5616be99bb0a5d46edc841b3d414d99d0d73d1a5
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-04-30 10:47:18 +0530

    minor fix in isPowerOfTwo

--- Recursion/isPowerOfTwo.js
+++ Recursion/isPowerOfTwo.js
@@ -15,7 +15,7 @@
 
 function isPowerOfTwo(n) {
     if (n == 1) return true;
-    else if (n > 1 || n % 2 !== 0)return false;
+    else if (n < 1 || n % 2 !== 0)return false;
     return isPowerOfTwo(n / 2)
 }
 console.log("Result :", isPowerOfTwo(10))
