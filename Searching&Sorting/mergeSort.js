8ba616c26094f9febca28e7b72bc036d6847b03f
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-05-05 22:56:53 +0530

    added math.floor

--- Searching&Sorting/mergeSort.js
+++ Searching&Sorting/mergeSort.js
@@ -19,7 +19,7 @@ function merge(left, right) {
 
 function mergeSort(arr) {
     if (arr.length <= 1) return arr;
-    let mid = arr.length / 2;
+    let mid = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, mid));
     let right = mergeSort(arr.slice(mid));
     return merge(left, right);
