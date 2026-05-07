2da588c5e105e69e43da8892b491c54560b00f36
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-05-07 19:00:21 +0530

    Linked list:addTohead, addToTail and addAtIndex method added

--- Linked List/linkdeListOperations.js
+++ Linked List/linkdeListOperations.js
@@ -3,7 +3,81 @@ function Node(value) {
     this.next = null;
 }
 
-function MyLinkdeList() {
+function MyLinkedList() {
     this.head = null;
     this.size = 0;
 }
+MyLinkedList.prototype.addToHead  = function(value) {
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+}
+
+MyLinkedList.prototype.addToTail = function(value) {
+    let newNode = new Node(value);
+    this.size++; 
+
+    if (!this.head) {
+        this.head = newNode;
+        return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+        current = current.next;
+    }
+    current.next = newNode;
+};
+
+MyLinkedList.prototype.addAtIndex = function (index, value) {
+    let newNode = new Node(value);
+    if (index == 0) {
+        this.addToHead(value);
+        return;
+    }
+    else if (index == this.size) {
+        this.addToTail(value);
+        return;
+    } else {
+        this.size++; // Keep the counter accurate!
+        let i = 0;
+        let current = this.head;
+        while (i < index - 1) {
+            current = current.next;
+            i++;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+};
+
+MyLinkedList.prototype.printList = function() {
+    let current = this.head;
+    let result = "";
+
+    while (current) {
+        result += current.value + " -> ";
+        current = current.next; // Move to the next node
+    }
+
+    console.log(result + "null");
+}
+
+let node1 = new Node(5);
+let node2 = new Node(10);
+let node3 = new Node(15);
+node1.next = node2;
+node2.next = node3;
+
+let list = new MyLinkedList();
+
+list.head = node1;
+list.size = 3;
+list.addToHead(25);
+list.addToTail(100);
+list.printList();
+console.log("List size:", list.size);
+list.addAtIndex(2, 50);
+list.printList();
+console.log("List size:", list.size);
