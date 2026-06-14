e256eb0b263a0733ada12f3d1603d7832212667d
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-06-14 14:08:55 +0530

    Strings : length of last word: third  approach

--- Strings/lengthOfLastworld.js
+++
@@ -1,7 +0,0 @@
-function lengthOfLastWord(s){
-    s= s.trim();
-    s= s.split(" ");
-    return s[s.length -1].length;
-}
-let str = " Fly me to  the moon ";
-console.log(lengthOfLastWord(str)); // Output: 5
