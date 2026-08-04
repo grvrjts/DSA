04d39580ad36c6d56017b774ea6ceda22eedb2bf
GAURAV RAJPUT <gauravrajput.developer@gmail.com>
2026-08-04 10:04:56 +0530

    group anagram second approach

--- Strings/groupAnagrams.js
+++ Strings/groupAnagrams.js
@@ -5,7 +5,7 @@ This approach uses a sorted version of each string as a unique key to group anag
 
 let strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
 
-function groupdAnagrams(strs) {
+function groupAnagrams(strs) {
     let map = {};
     for (let i = 0; i < strs.length; i++) {
         let str = strs[i].split('').sort().join('');
@@ -18,10 +18,41 @@ function groupdAnagrams(strs) {
     return Object.values(map);
 }
 
-console.log("Result: ", groupdAnagrams(strs))
+console.log("Result: ", groupAnagrams(strs))
 
 /*
 Time & Space Complexity
 Time Complexity: O(n·k·log k), where n is the number of strings and k is the average length of each string (due to sorting)
 Space Complexity: O(n·k), for storing grouped anagrams.
 */
+
+// Another approach : Hashed Key 
+
+function groupAnagrams1(strs) {
+    let map = {};
+
+    for (let i = 0; i < strs.length; i++) {
+        let freqArray = Array(26).fill(0);
+        let s = strs[i];
+        console.log("s: ", s);
+        for (let j = 0; j < s.length; j++) {
+            let index = s[j].charCodeAt() - 'a'.charCodeAt();
+            ++freqArray[index];
+        }
+        console.log("freqArray: ", freqArray);
+        let key = "";
+        for (let k = 0; k < 26; k++) {
+            key = key + "#" + freqArray[k];
+            console.log("key: ", key);
+        }
+
+        if (!map[key]) {
+            map[key] = [s];
+        } else {
+            map[key].push(s);
+        }
+    }
+    return Object.values(map);
+}
+
+console.log("Result 1: ", groupAnagrams1(strs))
